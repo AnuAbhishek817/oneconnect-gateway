7362f7784c8a07d9e70aa3dcf8466efd11c98448
AnuAbhishek817 <160491072+AnuAbhishek817@users.noreply.github.com>
2025-07-18 13:57:30 +0530

    Add files via upload

---
+++ all.sh
@@ -0,0 +1,9 @@
+
+      sudo apt install libxcb-cursor0 libxcb-cursor-dev
+      sudo apt install git
+      sudo apt install clang
+      sudo apt install gcc
+      sudo apt install g++
+      sudo apt install python-is-python3
+      sudo apt-get install libmodbus5 openssl libcurl4
+      
